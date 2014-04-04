f998e8b8620fec11343c28585c8106b7003a5573
Emil Stenqvist <emsten@gmail.com>
2014-04-04 18:20:49 +0200

    Made PDFReader a class instead

---
+++ pdfreader.py
@@ -0,0 +1,45 @@
+import sys
+import io
+
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter, HTMLConverter
+from pdfminer.layout import LAParams
+from pdfminer.image import ImageWriter
+
+class UnicodeTextConverter(TextConverter):
+    def write_text(self, text):
+        self.outfp.write(text.decode(self.codec))
+
+class PDFReader(object):
+    """ PDFReader - convert PDF's to text. """
+
+    def __init__(self, outfp=sys.stdout):
+
+        self.rsrcmgr = PDFResourceManager()
+        self.device = UnicodeTextConverter(self.rsrcmgr, outfp)
+
+    def read(self, filename):
+        """ Reads the given PDF and outputs text to the supplied outfp """
+
+        pagenos     = set()
+        maxpages    = 0
+        layoutmode  = 'normal'
+        scale       = 1
+
+        with file(filename, 'rb') as fp:
+
+            interpreter = PDFPageInterpreter(self.rsrcmgr, self.device)
+            for page in PDFPage.get_pages(fp, pagenos, check_extractable=True):
+                interpreter.process_page(page)
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+
+    stream = io.StringIO()
+
+    reader = PDFReader(outfp=stream)
+    reader.read(filename)
+
+    print stream.getvalue()
