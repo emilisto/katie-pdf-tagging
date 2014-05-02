c793faac63e6601047a7567e0db9228cbc126b1b
Emil Stenqvist <emsten@gmail.com>
2014-05-02 16:08:48 +0200

    Return a generator instead of a string

--- pdfreader.py
+++ pdfreader.py
@@ -13,8 +13,8 @@ class UnicodeTextConverter(TextConverter):
             text = text.decode(self.codec)
         self.outfp.write(text)
 
-def pdf_to_str(filename):
-    """ Takes a PDF filename and returns its contents in unicode """
+def read_pdf(filename):
+    """ Yields the content of a PDF """
 
     buf = io.StringIO()
 
@@ -25,12 +25,19 @@ def pdf_to_str(filename):
     with file(filename, 'rb') as fp:
         interpreter = PDFPageInterpreter(rsrcmgr, device)
         for page in PDFPage.get_pages(fp, set(), check_extractable=True):
+            buf.truncate(0)
             interpreter.process_page(page)
+            yield buf.getvalue()
 
-    return buf.getvalue()
+def pdf_to_str(filename):
+    """ Returns the contents of a PDF as a unicode string """
 
+    content = ''
+    for chunk in read_pdf(filename):
+        content += chunk
+    return content
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-    content = pdf_to_str(filename)
-    print content
+    for text in read_pdf(filename):
+        print text.encode('utf8')
